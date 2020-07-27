810d1c3d239269c5edd93abce19857bf4f239897
lukehayes <lukepurplehayes@gmail.com>
2020-07-27 13:10:54 +0100

    Created globals.h

---
+++ include/globals.h
@@ -0,0 +1,11 @@
+/**
+ * Global helper variables are defined in this file
+ */
+
+const int GRID_X = 20;
+const int GRID_Y = 20;
+const int CELL_W = 10;
+const int CELL_H = 10;
+const int SPACE = 11;
+const int SCR_W = 800;
+const int SCR_H = 600;
