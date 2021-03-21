275b997d45d513db5289443f42768b55af5cc768
Luke <lukepurplehayes@gmail.com>
2021-03-21 21:25:38 +0000

    main.c now loads simple image/texture

--- main.c
+++ main.c
@@ -1,14 +1,24 @@
 #include "raylib.h"
 #include "globals.h"
-#include "out.h"
+#include <math.h>
 
 
 int main(void)
 {
     InitWindow(SCR_W, SCR_H, "RayLib Game");
     SetTargetFPS(60);
+    
+    Image img = LoadImage("assets/bernie.png");
+    Texture2D texture = LoadTextureFromImage(img);
+    
+    static float x = 0;
+    static float y = 0;
+
 
     while (!WindowShouldClose() ) {
+
+        x += 0.1;
+        y += 0.1;
 
         ClearBackground(BLACK);
 
