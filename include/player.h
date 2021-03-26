5bae1f197aae42c8b9dfa4ce7c97641224a49303
Luke <lukepurplehayes@gmail.com>
2021-03-26 18:41:00 +0000

    Added Player creation and destruction functions.

--- include/player.h
+++ include/player.h
@@ -1,12 +1,35 @@
 #ifndef RL_PLAYER_H
 #define RL_PLAYER_H
 
+#include "raylib.h"
+#include <stdlib.h>
+
 typedef struct Player
 {
     int x;
     int y;
     int speed;
+    Color color;
 } Player;
+
+Player* CreatePlayer()
+{
+    Player* player = (Player*)malloc(sizeof(Player));
+    player->x = 10;
+    player->y = 10;
+    player->speed = 10;
+    player->color = ORANGE;
+
+    return player;
+}
+
+void DestroyPlayer(Player* player) 
+{
+    free(player);
+    TraceLog(LOG_INFO, "Player Object Freed");
+}
+
+
 
 #endif
 
--- main.c
+++ main.c
@@ -1,31 +1,38 @@
 #include "raylib.h"
 #include "globals.h"
+#include "input.h"
+#include "player.h"
 #include <math.h>
-
 
 int main(void)
 {
     InitWindow(SCR_W, SCR_H, "RayLib Game");
     SetTargetFPS(60);
     
-    Image img = LoadImage("assets/bernie.png");
-    Texture2D texture = LoadTextureFromImage(img);
-    
+    /*Image img = LoadImage("assets/bernie.png");*/
+    /*Texture2D texture = LoadTextureFromImage(img);*/
+
+    Player* p = CreatePlayer();
+
     static float x = 0;
     static float y = 0;
-
 
     while (!WindowShouldClose() ) {
 
         x += 0.1;
         y += 0.1;
+    
+        rlGetInput(p);
 
         ClearBackground(BLACK);
 
+
         BeginDrawing();
+            DrawRectangle(p->x,p->y,10,10, p->color);
         EndDrawing();
     }
 
+    DestroyPlayer(p);
     CloseWindow();
 
     return 0;
