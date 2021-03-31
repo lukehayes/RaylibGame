3ccd56ebf30dd066318dfe7ce8281453e1602d2d
lukehayes <lukepurplehayes@gmail.com>
2021-03-31 15:19:20 +0100

    Added Types.h to hold typedef'd types for portability.

---
+++ include/Types.h
@@ -0,0 +1,24 @@
+#ifndef LH_TYPE_H
+#define LH_TYPE_H
+
+#include "raylib.h"
+
+/*============================================================
+// Primitives
+============================================================*/
+typedef uint8_t       u8;
+typedef u8            b8;
+typedef int8_t        s8;
+typedef uint16_t      u16;
+typedef int16_t       s16;
+typedef uint32_t      u32;
+typedef int32_t       s32;
+typedef s32           b32;
+typedef uint64_t      u64;
+typedef int64_t       s64;
+typedef float         f32;
+typedef double        f64;
+typedef char*         str;
+typedef const char*   const_str;
+
+#endif // LH_TYPES_H
